17bd71bdfc2782df7be50ab8bb4b3a2875fdbc0e
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-01 16:45:39 +0000

    Updated Pawn initial movement, passes all tests

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,14 +10,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
+            int whiteStartSpace = 7;
+            int blackStartSpace = 1;
             Square currentSquare = board.FindPiece(this);
             List<Square> moves = new List<Square>();
             if (this.Player == Player.Black)
             {
                 moves.Add(Square.At((currentSquare.Row + 1), currentSquare.Col));
+                if (currentSquare.Row == blackStartSpace)
+                {
+                    moves.Add(Square.At(currentSquare.Row + 2, currentSquare.Col));
+                }
             } else
             {
                 moves.Add(Square.At((currentSquare.Row - 1), currentSquare.Col));
+                if (currentSquare.Row == whiteStartSpace)
+                {
+                    moves.Add(Square.At(currentSquare.Row - 2, currentSquare.Col));
+                }
             }
             return moves;
         }
