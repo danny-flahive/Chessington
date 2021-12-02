d46e810d815d4bb41789df7229033aa588179623
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-02 11:16:02 +0000

    Added Knight movement, passes all tests
    Lots of if statements are used in this (and other move functions) to prevent indexoutofbounds errors - though I don't know if that's possible in this program. Pretty defensive coding

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,56 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            int row = currentSquare.Row;
+            int col = currentSquare.Col;
+            if (row + 2 < 8)
+            {
+                if (col + 1 < 8)
+                {
+                    moves.Add(Square.At(row + 2, col + 1));
+                }
+                if (col - 1 > 0)
+                {
+                    moves.Add(Square.At(row + 2, col - 1));
+                }
+            }
+            if (row - 2 >= 0)
+            {
+                if (col + 1 < 8)
+                {
+                    moves.Add(Square.At(row - 2, col + 1));
+                }
+                if (col - 1 > 0)
+                {
+                    moves.Add(Square.At(row - 2, col - 1));
+                }
+            }
+            if (col + 2 < 8)
+            {
+                if (row + 1 < 8)
+                {
+                    moves.Add(Square.At(row + 1, col + 2));
+                }
+                if (col - 1 > 0)
+                {
+                    moves.Add(Square.At(row - 1, col + 2));
+                }
+            }
+            if (col - 2 >= 0)
+            {
+                if (row + 1 < 8)
+                {
+                    moves.Add(Square.At(row + 1, col - 2));
+                }
+                if (col - 1 > 0)
+                {
+                    moves.Add(Square.At(row - 1, col - 2));
+                }
+            }
+            
+            return moves;
         }
     }
 }
