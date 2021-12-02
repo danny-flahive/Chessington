3a534a27c3cef869bbc2a75a3e9380ba3888205d
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-02 10:49:33 +0000

    Bishop can move, all tests pass

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,37 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            int row = currentSquare.Row;
+            int col = currentSquare.Col;
+            for (int i = 1; i < 8; i++)
+            {
+                if (row - i >= 0)
+                {
+                    if (col - i >= 0)
+                    {
+                        moves.Add(Square.At(row - i, col - i));     //Move to upper left
+                    }
+                    if (col + i <= 7)
+                    {
+                        moves.Add(Square.At(row - i, col + i));     //Move to upper right
+                    }
+                }
+                if (row + i <= 7)
+                {
+                    if (col - i >= 0)
+                    {
+                        moves.Add(Square.At(row + i, col - i));     //Move to lower left
+                    }
+                    if (col + i <= 7)
+                    {
+                        moves.Add(Square.At(row + i, col + i));     //Move to lower right
+                    }
+                }
+            }
+
+            return moves;
         }
     }
 }
