b280d9565f95cb59f8aca6c944ac9949506866d2
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-02 14:45:46 +0000

    Pawns can now capture, all tests pass

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -14,20 +14,51 @@ namespace Chessington.GameEngine.Pieces
             List<Square> moves = new List<Square>();
             int row = currentSquare.Row;
             int col = currentSquare.Col;
-            if (this.Player == Player.Black && row + 1 < 8 && board.GetPiece(Square.At(row + 1, col)) == null)
+            if (this.Player == Player.Black && row + 1 < 8)
             {
-                moves.Add(Square.At(row + 1, col));
-                if (!this.Moved && row + 2 < 8 && board.GetPiece(Square.At(row + 2, col)) == null)
+                if (board.GetPiece(Square.At(row + 1, col)) == null)
                 {
-                    moves.Add(Square.At(row + 2, col));
+                    moves.Add(Square.At(row + 1, col));
+                    if (!this.Moved && row + 2 < 8 && board.GetPiece(Square.At(row + 2, col)) == null)
+                    {
+                        moves.Add(Square.At(row + 2, col));
+                    }
                 }
-            } else if (this.Player == Player.White && row - 1 >= 0 && board.GetPiece(Square.At(row - 1, col)) == null)
+                if (col + 1 < 8 && board.GetPiece(Square.At(row + 1, col + 1)) != null && board.GetPiece(Square.At(row + 1, col + 1)).Player != this.Player)
+                {
+                    moves.Add(Square.At(row + 1, col + 1));
+                }
+                if (col - 1 >= 0 && board.GetPiece(Square.At(row + 1, col - 1)) != null && board.GetPiece(Square.At(row + 1, col - 1)).Player != this.Player)
+                {
+                    moves.Add(Square.At(row + 1, col - 1));
+                }
+            } else if (this.Player == Player.White && row - 1 >= 0)
             {
-                moves.Add(Square.At(row - 1, col));
-                if (!this.Moved && row - 2 >= 0 && board.GetPiece(Square.At(row - 2, col)) == null)
+                if (board.GetPiece(Square.At(row - 1, col)) == null)
                 {
-                    moves.Add(Square.At(row - 2, col));
+                    moves.Add(Square.At(row - 1, col));
+                    if (!this.Moved && row - 2 >= 0 && board.GetPiece(Square.At(row - 2, col)) == null)
+                    {
+                        moves.Add(Square.At(row - 2, col));
+                    }
                 }
+                if (col + 1 < 8 && board.GetPiece(Square.At(row - 1, col + 1)) != null && board.GetPiece(Square.At(row - 1, col + 1)).Player != this.Player)
+                {
+                    moves.Add(Square.At(row - 1, col + 1));
+                }
+                if (col - 1 >= 0 && board.GetPiece(Square.At(row - 1, col - 1)) != null && board.GetPiece(Square.At(row - 1, col - 1)).Player != this.Player)
+                {
+                    moves.Add(Square.At(row - 1, col - 1));
+                }
+            }
+
+            if (col + 1 < 8)
+            {
+                
+            }
+            if (col - 1 >= 0)
+            {
+
             }
             return moves;
         }
