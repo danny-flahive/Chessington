33009535b659ddc59f7ea4d3a01af65b7fd64204
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-02 10:57:23 +0000

    Queen can move, all tests pass

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,52 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            int row = currentSquare.Row;
+            int col = currentSquare.Col;
+
+            //Diagonal movement
+            for (int i = 1; i < 8; i++)
+            {
+                if (row - i >= 0)
+                {
+                    if (col - i >= 0)
+                    {
+                        moves.Add(Square.At(row - i, col - i));     //Move to upper left
+                    }
+                    if (col + i <= 7)
+                    {
+                        moves.Add(Square.At(row - i, col + i));     //Move to upper right
+                    }
+                }
+                if (row + i <= 7)
+                {
+                    if (col - i >= 0)
+                    {
+                        moves.Add(Square.At(row + i, col - i));     //Move to lower left
+                    }
+                    if (col + i <= 7)
+                    {
+                        moves.Add(Square.At(row + i, col + i));     //Move to lower right
+                    }
+                }
+            }
+            //Lateral movement
+            for (int i = 0; i < 8; i++)
+            {
+                if (currentSquare.Row != i)
+                {
+                    moves.Add(Square.At(i, currentSquare.Col));
+                }
+                if (currentSquare.Col != i)
+                {
+                    moves.Add(Square.At(currentSquare.Row, i));
+                }
+            }
+
+
+            return moves;
         }
     }
 }
