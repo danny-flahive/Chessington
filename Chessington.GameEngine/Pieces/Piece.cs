84bcc86a62aefdd47584c20fe97de929ea9b40f5
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-02 12:47:28 +0000

    Changed LateralMovement, passes all Rook tests

--- Chessington.GameEngine/Pieces/Piece.cs
+++ Chessington.GameEngine/Pieces/Piece.cs
@@ -30,18 +30,56 @@ namespace Chessington.GameEngine.Pieces
         {
             List<Square> moves = new List<Square>();
             Square currentSquare = board.FindPiece(this);
-            for (var i = 0; i < 8; i++)
+            ////Prevent this from passing through pieces
+            //for (var i = 0; i < 8; i++)
+            //{
+            //    if (currentSquare.Row != i)
+            //    {
+            //        moves.Add(Square.At(i, currentSquare.Col));
+            //    }
+            //    if (currentSquare.Col != i)
+            //    {
+            //        moves.Add(Square.At(currentSquare.Row, i));
+            //    }
+            //}
+            //return moves;
+            int row = currentSquare.Row;
+            int col = currentSquare.Col;
+            for (int i = 1; i < 8; i++)
             {
-                if (currentSquare.Row != i)
+                if (!(row + i < 8 && board.GetPiece(Square.At(row + i, col)) == null))
                 {
-                    moves.Add(Square.At(i, currentSquare.Col));
+                    break;
                 }
-                if (currentSquare.Col != i)
-                {
-                    moves.Add(Square.At(currentSquare.Row, i));
-                }
+                moves.Add(Square.At(row + i, col));
             }
+            for (int i = 1; i < 8; i++)
+            {
+                if (!(row - i >= 0 && board.GetPiece(Square.At(row - i, col)) == null))
+                {
+                    break;
+                }
+                moves.Add(Square.At(row - i, col));
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                if (!(col + i < 8 && board.GetPiece(Square.At(row, col + i)) == null))
+                {
+                    break;
+                }
+                moves.Add(Square.At(row, col + i));
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                if (!(col - i >= 0 && board.GetPiece(Square.At(row, col - i)) == null))
+                {
+                    break;
+                }
+                moves.Add(Square.At(row, col - i));
+            }
+            //moves.RemoveAll(s => s == Square.At(row, col));
             return moves;
+
         }
 
         public IEnumerable<Square> GetDiagonalMovement(Board board)
