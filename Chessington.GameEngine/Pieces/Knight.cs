2d67201513484b4fb496625324ae84bb637f473f
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-02 15:42:31 +0000

    Refactored Knight similarly to King

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,77 +10,26 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            //TODO: Add offset array
+            int[,] offsets = new int[,]
+            {
+                {2, 1},
+                {2, -1},
+                {-2, 1},
+                {-2, -1},
+                {1, 2},
+                {-1, 2},
+                {1, -2},
+                {-1, -2}
+            };
             List<Square> moves = new List<Square>();
             Square currentSquare = board.FindPiece(this);
             int row = currentSquare.Row;
             int col = currentSquare.Col;
-            if (row + 2 < 8)
+            for (int i = 0; i < offsets.GetLength(0); i++)
             {
-                if (col + 1 < 8)
+                if (ValidMoveSpace(board, Square.At(row + offsets[i, 0], col + offsets[i, 1])))
                 {
-                    if(ValidMoveSpace(board, Square.At(row + 2, col + 1)))
-                    {
-                        moves.Add(Square.At(row + 2, col + 1));
-                    }
-                }
-                if (col - 1 > 0)
-                {
-                    if (ValidMoveSpace(board, Square.At(row + 2, col - 1)))
-                    {
-                        moves.Add(Square.At(row + 2, col - 1));
-                    }
-                }
-            }
-            if (row - 2 >= 0)
-            {
-                if (col + 1 < 8)
-                {
-                    if (ValidMoveSpace(board, Square.At(row - 2, col + 1)))
-                    {
-                        moves.Add(Square.At(row - 2, col + 1));
-                    }
-                }
-                if (col - 1 > 0)
-                {
-                    if (ValidMoveSpace(board, Square.At(row - 2, col - 1)))
-                    {
-                        moves.Add(Square.At(row - 2, col - 1));
-                    }
-                }
-            }
-            if (col + 2 < 8)
-            {
-                if (row + 1 < 8)
-                {
-                    if (ValidMoveSpace(board, Square.At(row + 1, col + 2)))
-                    {
-                        moves.Add(Square.At(row + 1, col + 2));
-                    }
-                }
-                if (row - 1 > 0)
-                {
-                    if (ValidMoveSpace(board, Square.At(row - 1, col + 2)))
-                    {
-                        moves.Add(Square.At(row - 1, col + 2));
-                    }
-                }
-            }
-            if (col - 2 >= 0)
-            {
-                if (row + 1 < 8)
-                {
-                    if (ValidMoveSpace(board, Square.At(row + 1, col - 2)))
-                    {
-                        moves.Add(Square.At(row + 1, col - 2));
-                    }
-                }
-                if (row - 1 > 0)
-                {
-                    if (ValidMoveSpace(board, Square.At(row - 1, col - 2)))
-                    {
-                        moves.Add(Square.At(row - 1, col - 2));
-                    }
+                    moves.Add(Square.At(row + offsets[i, 0], col + offsets[i, 1]));
                 }
             }
             return moves;
